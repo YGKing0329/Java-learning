b6af0a9185ee3203f93bd225b41ca21b96f467e5
YGKing0329 <647219529@qq.com>
2026-02-19 11:43:48 +0800

    Add files via upload
    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package com.hunter.test;
+
+public class HelloWorld {
+    //    单行注释快捷键ctrl+/
+    /*多行注释,先选中这些文本
+       再ctrl+shift+/  */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("hallo word");
+    }
+}
+
